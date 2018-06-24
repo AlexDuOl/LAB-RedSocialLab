6704ad7553b58850cefb946b13c87591f503d444
antaresvalle <antaresvll561@gmail.com>
2018-06-24 17:49:37 -0500

    se agrega funcionalidad para subir imagen; para agregar fecha; se vuelven requeridos campos de foto y ubicacion y se limpian campos de inputs de modal

--- js/app.js
+++ js/app.js
@@ -7,9 +7,23 @@ var experiencia;
 var costoViaje;
 var userImg;
 var username;
+var date = moment().format("h:mm a, DD/MM/YYYY");
+var file;
+var reader;
+var dataURL;
 
-function getDataPost(){
-	foto = $('#foto-viaje').val();
+function imgUrl(fileInput) {
+	reader = new FileReader();
+	reader.onload = function(evt) {
+		dataURL = evt.target.result;
+		addPostData();
+	}
+
+	reader.readAsDataURL(fileInput.files[0]);
+}
+
+function getPostData(){
+	foto = $('#foto-viaje')[0];
 	ubicacion = $('#input-place').val();
 	servicio = $('#hospdaje-opt').val();/*checked*/
 	costoServicio = $('#costo-serv').val();
@@ -17,9 +31,22 @@ function getDataPost(){
 	costoViaje = $('#costo-viaje').val();
 	userImg = 'placeholder';
 	username = 'placeholder';
-	console.log( foto + ubicacion + servicio + costoServicio + experiencia + costoViaje);
 
-	addPostData();
+	if(foto.value && ubicacion){
+		imgUrl(foto);
+	} else {
+		$('.file-path-wrapper').attr('style', 'background:red;');
+		$('#input-place').attr('style', 'background:red;');
+		console.log('missing information');
+	}
+
+	$('.file-path').val('');
+	$('#input-place').val('');
+	$('#hospdaje-opt').val('');/*checked*/
+	$('#costo-serv').val('');
+	$('#text-experiencia').val('');
+	$('#costo-viaje').val('');
+	
 };
 
 
@@ -47,7 +74,7 @@ function addPostData(){
         </div>   
       </section><!--SEGUNDA SECCIÓN Fotos-->
       <section class="row">
-        <img id="cont-foto-viaje" class="responsive-img viaje-img" src="${foto}" alt="">
+        <img id="cont-foto-viaje" class="responsive-img viaje-img" src="${dataURL}" alt="">
       </section>
       <section><!--TERCERA SECCIÓN Iconos-->
         <div class="row no-margin">
@@ -68,18 +95,19 @@ function addPostData(){
             </div>
           </div>
           <div class="col s6">
-            <p class="right">Fecha</p>
+            <p class="right">${date}</p>
           </div>
         </div>
       </section>
     </div>
   </div>`
-	$('main').append(postTemplate);
+ $('main').append(postTemplate);
+ $('.modal').modal('close');
 }
 
 
 $(document).ready(function(){
     $('.modal').modal();
-    btnAdd.click(getDataPost);
+    btnAdd.click(getPostData);
     $('select').formSelect();
 });
