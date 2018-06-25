ba65f462ce67287146e8e6a4870bfc39d6cfc060
AlexDuOl <contactogatovagabundo@gmail.com>
2018-06-25 12:14:40 -0500

    Ajustes en index.html

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
     // firebase
     var config = {
@@ -11,29 +10,5 @@ $(document).ready(function() {
     };
     firebase.initializeApp(config);
   
-  var finalTemplate = "";
-  finalTemplate = template.replace('__img1__', imageUser);
-  $('section').append(finalTemplate);
-  
-  var imageUser;
-
-  firebase.database().ref("posts")
-  .on("child_added", function(s){
-    var user = s.val();
-    imageUser = user.imgv; 
-    console.log(imageUser);
-});
-
-
-var template = '<table class="container">'+
-                  '<tbody>'+
-                    '<tr class="row">'+
-                      '<td class="col s4">__img1__</td>'+
-                      '<td class="col s4">__img2__</td>'+
-                      '<td class="col s4">__img3__</td>'+
-                    '</tr>'+
-                  '</tbody>'+
-                '</table>'+
-
 });
 
