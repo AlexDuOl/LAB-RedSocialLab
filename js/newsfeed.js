5eb29e01f499c40df8eb4b1622fe0d72c3eeedb7
antaresvalle <antaresvll561@gmail.com>
2018-06-25 10:51:18 -0500

    se eliminan varibales no usadas, se cambia el uso de parametros que se asignan al post en vez de varibales

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	// Se eliminaron algunas variables que no se estaban utilizando
+	// Variables on TOP
 	var displayName;
     var email;
     var emailVerified;
@@ -6,8 +8,30 @@ $(document).ready(function() {
     var isAnonymous;
     var uid;
     var providerData;
+    var authorAvatar;
 
-	// datos de usuario
+    var btnAdd = $('#add-post');
+	var foto;
+	var ubicacion;
+	var servicio;/*checked*/
+	var costoServicio;
+	var experiencia;
+	var costoViaje;
+	var date = moment().format("h:mm a, DD/MM/YYYY");
+	var reader;
+	var dataURL;
+	
+	// leer base de datos
+
+	firebase.database().ref("posts")
+	.on("child_added", function(snapshot){
+			var newPost = snapshot.val();
+			// addPostData ahora acepta parametros pues no es necesario agregar los post en el click del boton
+			// Y agrega el post igual que los post ya existentes
+			addPostData(newPost.authorAvatar, newPost.nombre, newPost.ubicacion, newPost.imgv, newPost.fecha);
+	});
+
+	    // datos de usuario
 	firebase.auth().onAuthStateChanged(function(user) {
 		if (user) {
 		    // User is signed in.
@@ -18,59 +42,46 @@ $(document).ready(function() {
 			isAnonymous = user.isAnonymous;
 			uid = user.uid;
 			providerData = user.providerData;
+			
 		} else {
 		    window.location = "403.html";
 		}
 	});
 
-	// leer base de datos
-	firebase.database().ref("posts")
-	.on("child_added", function(snapshot){
-		var newPost = snapshot.val();
-		dataURL = newPost.imgv;
-		displayName = newPost.nombre;
-		ubicacion =newPost.ubicacion;
-		date = newPost.fecha;
-
-		addPostData()
-		console.log(newPost);
-	});
-
-	var btnAdd = $('#add-post');
-	var foto;
-	var ubicacion;
-	var servicio;/*checked*/
-	var costoServicio;
-	var experiencia;
-	var costoViaje;
-	var userImg;
-	var username;
-	var date = moment().format("h:mm a, DD/MM/YYYY");
-	var file;
-	var reader;
-	var dataURL;
 
 	function imgUrl(fileInput) {
 		reader = new FileReader();
 		reader.onload = function(evt) {
 			dataURL = evt.target.result;
-			// addPostData();
+			// SE removio addpost de aqui pues lo hace en el evento de firebase
 			savePostData();
 		}
 
 		reader.readAsDataURL(fileInput.files[0]);
 	}
 
+	// escribir en base de datos
+	function savePostData () {
+		var post = {
+				nombre:displayName,
+				ubicacion:ubicacion,
+				imgv:dataURL,
+				fecha:date,
+				ownerID:uid,
+				authorAvatar: photoURL ,
+			};
+
+		firebase.database().ref("posts")
+		.push(post);
+	}
+
 	function getPostData(){
-		console.log('click');
 		foto = $('#foto-viaje')[0];
 		ubicacion = $('#input-place').val();
-		servicio = $('#hospdaje-opt').val();/*checked*/
+		servicio = $('#hospdaje-opt').val();
 		costoServicio = $('#costo-serv').val();
 		experiencia = $('#text-experiencia').val();
 		costoViaje = $('#costo-viaje').val();
-		userImg = 'placeholder';
-		username = 'placeholder';
 
 		if(foto.value && ubicacion){
 			imgUrl(foto);
@@ -88,25 +99,23 @@ $(document).ready(function() {
 		$('#costo-viaje').val('');
 		
 	};
-
-
-
-	function addPostData(){
+	// Se opto por parametros en lugar de variables
+	function addPostData(authorAvatar, authorName, location, imageURI, datePublished){
 		var postTemplate = `<div class="row">
 	    <div class="col s12 m8 l8 post-card">
 	      <section><!--PRIMERA SECCIÓN Datos de usuario-->
 	        <div class="row usuario-row no-margin">
 	          <div class="col s3 m2">
-	            <img id="user-photo" class="circle usuario-m" src="${photoURL}" alt="">
+	            <img id="user-photo" class="circle usuario-m" src="${authorAvatar}" alt="">
 	          </div>
 	          <div class="col s9 m8 l10">
-	            <p id="user-name">${displayName}</p>
+	            <p id="user-name">${authorName}</p>
 	            <div class="row no-margin">
 	              <div class="col s1">
 	                <i class="material-icons">place</i>
 	              </div>
 	              <div class="col s10">
-	                <p id="cont-place">${ubicacion}</p>
+	                <p id="cont-place">${location}</p>
 	              </div>
 	            </div>
 	            <a id="mas-info" href="#">Mas info del viaje</a>
@@ -114,7 +123,7 @@ $(document).ready(function() {
 	        </div>   
 	      </section><!--SEGUNDA SECCIÓN Fotos-->
 	      <section class="row">
-	        <img id="cont-foto-viaje" class="responsive-img viaje-img" src="${dataURL}" alt="">
+	        <img id="cont-foto-viaje" class="responsive-img viaje-img" src="${imageURI}" alt="">
 	      </section>
 	      <section><!--TERCERA SECCIÓN Iconos-->
 	        <div class="row no-margin">
@@ -135,7 +144,7 @@ $(document).ready(function() {
 	            </div>
 	          </div>
 	          <div class="col s6">
-	            <p class="right">${date}</p>
+	            <p class="right">${datePublished}</p>
 	          </div>
 	        </div>
 	      </section>
@@ -145,30 +154,8 @@ $(document).ready(function() {
 	 $('.modal').modal('close');
 	}
 
-	console.log(dataURL);
-
-	// escribir en base de datos
-	function savePostData () {
-		var post = {
-				nombre:displayName,
-				ubicacion:ubicacion,
-				imgv:dataURL,
-				fecha:date,
-				ownerID:uid
-			};
-
-		firebase.database().ref("posts")
-		.push(post);
-	}
-
 
 	$('.modal').modal();
 	btnAdd.click(getPostData);
 	$('select').formSelect();
-
-	
-
-
-
-
 });
