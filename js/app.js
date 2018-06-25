3be579cea82a682be14a1515af6f04ce6e9243c7
AlexDuOl <contactogatovagabundo@gmail.com>
2018-06-25 00:11:17 -0500

    Subiendo ajustes a vista de usuarios y funcionalidad en js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+window.onload = inicializar;
+var fichero;
+var storageRef;
 
+function inicializar (){
+    fichero = document.getElementById("newImage");
+    fichero.addEventListener("change", uploadImages, false);
+
+    storageRef = firebase.storage().ref();
+};
+
+function uploadImages(){
+    var uploadImage = fichero.files[0];
+
+    var uploadTask = storageRef.child('images/' + uploadImage.name).put(uploadImage);
+};
