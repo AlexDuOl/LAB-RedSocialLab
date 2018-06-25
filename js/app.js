82338140add1e92882513f5f41800dd355ebaac2
antaresvalle <antaresvll561@gmail.com>
2018-06-24 23:21:52 -0500

    se agrega firebase. Se crean archivos login.js, newsfeed.js y 403.html. Se obtiene nombre e imagen de perfil de usuario loggeado y se muestran en el post

--- js/app.js
+++ js/app.js
@@ -1,113 +1,13 @@
-var btnAdd = $('#add-post');
-var foto;
-var ubicacion;
-var servicio;/*checked*/
-var costoServicio;
-var experiencia;
-var costoViaje;
-var userImg;
-var username;
-var date = moment().format("h:mm a, DD/MM/YYYY");
-var file;
-var reader;
-var dataURL;
-
-function imgUrl(fileInput) {
-	reader = new FileReader();
-	reader.onload = function(evt) {
-		dataURL = evt.target.result;
-		addPostData();
-	}
-
-	reader.readAsDataURL(fileInput.files[0]);
-}
-
-function getPostData(){
-	foto = $('#foto-viaje')[0];
-	ubicacion = $('#input-place').val();
-	servicio = $('#hospdaje-opt').val();/*checked*/
-	costoServicio = $('#costo-serv').val();
-	experiencia = $('#text-experiencia').val();
-	costoViaje = $('#costo-viaje').val();
-	userImg = 'placeholder';
-	username = 'placeholder';
-
-	if(foto.value && ubicacion){
-		imgUrl(foto);
-	} else {
-		$('.file-path-wrapper').attr('style', 'background:red;');
-		$('#input-place').attr('style', 'background:red;');
-		console.log('missing information');
-	}
-
-	$('.file-path').val('');
-	$('#input-place').val('');
-	$('#hospdaje-opt').val('');/*checked*/
-	$('#costo-serv').val('');
-	$('#text-experiencia').val('');
-	$('#costo-viaje').val('');
-	
-};
-
-
-
-function addPostData(){
-	var postTemplate = `<div class="row">
-    <div class="col s12 m8 l8 post-card">
-      <section><!--PRIMERA SECCIÓN Datos de usuario-->
-        <div class="row usuario-row no-margin">
-          <div class="col s3 m2">
-            <img id="user-photo" class="circle usuario-m" src="${userImg}" alt="">
-          </div>
-          <div class="col s9 m8 l10">
-            <p id="user-name">${username}</p>
-            <div class="row no-margin">
-              <div class="col s1">
-                <i class="material-icons">place</i>
-              </div>
-              <div class="col s10">
-                <p id="cont-place">${ubicacion}</p>
-              </div>
-            </div>
-            <a id="mas-info" href="#">Mas info del viaje</a>
-          </div>
-        </div>   
-      </section><!--SEGUNDA SECCIÓN Fotos-->
-      <section class="row">
-        <img id="cont-foto-viaje" class="responsive-img viaje-img" src="${dataURL}" alt="">
-      </section>
-      <section><!--TERCERA SECCIÓN Iconos-->
-        <div class="row no-margin">
-          <div class="col s6">
-            <div class="row">
-              <div class="col s3">
-                <a href="#"><i class="material-icons">favorite_border</i></a>
-              </div>
-              <div class="col s3">
-                <a href="#"><i class="material-icons">insert_comment</i></a>
-              </div>
-              <div class="col s3">
-                <a href="#"><i class="material-icons">share</i></a>
-              </div>
-              <div class="col s3">
-                <a href="#">#Likes</a>
-              </div>
-            </div>
-          </div>
-          <div class="col s6">
-            <p class="right">${date}</p>
-          </div>
-        </div>
-      </section>
-    </div>
-  </div>`
- $('main').append(postTemplate);
- $('.modal').modal('close');
-}
-
-
-$(document).ready(function(){
-    $('.modal').modal();
-    btnAdd.click(getPostData);
-    $('select').formSelect();
+$(document).ready(function() {
+    // firebase
+    var config = {
+    	apiKey: "AIzaSyApDpoaeUms46ufKn1l0Esp-4IvEPuaNyA",
+    	authDomain: "vive-mexico-f7c49.firebaseapp.com",
+    	databaseURL: "https://vive-mexico-f7c49.firebaseio.com",
+    	projectId: "vive-mexico-f7c49",
+    	storageBucket: "vive-mexico-f7c49.appspot.com",
+    	messagingSenderId: "489916603655"
+    };
+    firebase.initializeApp(config);
 });
+
---
+++ js/newsfeed.js
@@ -0,0 +1,137 @@
+$(document).ready(function() {
+	var displayName;
+    var email;
+    var emailVerified;
+    var photoURL;
+    var isAnonymous;
+    var uid;
+    var providerData;
+
+	// datos de usuario
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+		    // User is signed in.
+		   	displayName = user.displayName;
+			email = user.email;
+			emailVerified = user.emailVerified;
+			photoURL = user.photoURL;
+			isAnonymous = user.isAnonymous;
+			uid = user.uid;
+			providerData = user.providerData;
+		} else {
+		    window.location = "403.html";
+		}
+	});
+
+	var btnAdd = $('#add-post');
+	var foto;
+	var ubicacion;
+	var servicio;/*checked*/
+	var costoServicio;
+	var experiencia;
+	var costoViaje;
+	var userImg;
+	var username;
+	var date = moment().format("h:mm a, DD/MM/YYYY");
+	var file;
+	var reader;
+	var dataURL;
+
+	function imgUrl(fileInput) {
+		reader = new FileReader();
+		reader.onload = function(evt) {
+			dataURL = evt.target.result;
+			addPostData();
+		}
+
+		reader.readAsDataURL(fileInput.files[0]);
+	}
+
+	function getPostData(){
+		console.log('click');
+		foto = $('#foto-viaje')[0];
+		ubicacion = $('#input-place').val();
+		servicio = $('#hospdaje-opt').val();/*checked*/
+		costoServicio = $('#costo-serv').val();
+		experiencia = $('#text-experiencia').val();
+		costoViaje = $('#costo-viaje').val();
+		userImg = 'placeholder';
+		username = 'placeholder';
+
+		if(foto.value && ubicacion){
+			imgUrl(foto);
+		} else {
+			$('.file-path-wrapper').attr('style', 'background:red;');
+			$('#input-place').attr('style', 'background:red;');
+			console.log('missing information');
+		}
+
+		$('.file-path').val('');
+		$('#input-place').val('');
+		$('#hospdaje-opt').val('');/*checked*/
+		$('#costo-serv').val('');
+		$('#text-experiencia').val('');
+		$('#costo-viaje').val('');
+		
+	};
+
+
+
+	function addPostData(){
+		var postTemplate = `<div class="row">
+	    <div class="col s12 m8 l8 post-card">
+	      <section><!--PRIMERA SECCIÓN Datos de usuario-->
+	        <div class="row usuario-row no-margin">
+	          <div class="col s3 m2">
+	            <img id="user-photo" class="circle usuario-m" src="${photoURL}" alt="">
+	          </div>
+	          <div class="col s9 m8 l10">
+	            <p id="user-name">${displayName}</p>
+	            <div class="row no-margin">
+	              <div class="col s1">
+	                <i class="material-icons">place</i>
+	              </div>
+	              <div class="col s10">
+	                <p id="cont-place">${ubicacion}</p>
+	              </div>
+	            </div>
+	            <a id="mas-info" href="#">Mas info del viaje</a>
+	          </div>
+	        </div>   
+	      </section><!--SEGUNDA SECCIÓN Fotos-->
+	      <section class="row">
+	        <img id="cont-foto-viaje" class="responsive-img viaje-img" src="${dataURL}" alt="">
+	      </section>
+	      <section><!--TERCERA SECCIÓN Iconos-->
+	        <div class="row no-margin">
+	          <div class="col s6">
+	            <div class="row">
+	              <div class="col s3">
+	                <a href="#"><i class="material-icons">favorite_border</i></a>
+	              </div>
+	              <div class="col s3">
+	                <a href="#"><i class="material-icons">insert_comment</i></a>
+	              </div>
+	              <div class="col s3">
+	                <a href="#"><i class="material-icons">share</i></a>
+	              </div>
+	              <div class="col s3">
+	                <a href="#">#Likes</a>
+	              </div>
+	            </div>
+	          </div>
+	          <div class="col s6">
+	            <p class="right">${date}</p>
+	          </div>
+	        </div>
+	      </section>
+	    </div>
+	  </div>`
+	 $('main').append(postTemplate);
+	 $('.modal').modal('close');
+	}
+
+	$('.modal').modal();
+	btnAdd.click(getPostData);
+	$('select').formSelect();
+});
